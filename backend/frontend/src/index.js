a560a73627942d49c1f95f763989de3cfcdac83e
saisravanik <skuchibh@purdue.edu>
2022-02-08 14:22:17 -0500

    added basic login page to frontend

--- backend/frontend/src/index.js
+++ backend/frontend/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import App from "./App";
+import LoginPage from "./LoginPage";
+
+const rootElement = document.getElementByID("root");
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+    <Route exact path='/' element={<App/>}/>
+    <Route path="/LoginPage" element={<LoginPage/>}</Route> 
+    </Routes>
+    </BrowserRouter>,
+  rootElement
 );

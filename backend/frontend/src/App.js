759d0a693ce89500b90ff3a6fedd8f575abdde48
saisravanik <skuchibh@purdue.edu>
2022-02-08 14:22:17 -0500

    added basic login page to frontend

--- backend/frontend/src/App.js
+++ backend/frontend/src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
-import Header from './components/header.js';
+import React from "react";
+import {Link } from "react-router-dom";
 
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      MYAPP
-    </div>
-  );
+ function App() {
+     return (
+       <div>
+         <p>
+	    PurdueCircle
+	    <br />
+	     Click the button below to Login ~
+	  </p>
+	  <Link to="/LoginPage"><button>
+	     Login
+	  </button>
+	  </Link>
+	 </div>
+      );
 }
 
 export default App;
---
+++ backend/frontend/src/Page2.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Page2() {
+	return (
+	    <div>
+		<p>Login Page Here</p>
+	    </div>
+	 );
+}
+
+export default Page2;

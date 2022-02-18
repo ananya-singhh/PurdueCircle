af6ab03e91e0f1900eccede9c9e93ee45ffd09aa
Ananya Singh <35239520+ananya-singhh@users.noreply.github.com>
2022-02-17 23:39:21 -0500

    added basic function headers to db_interface

--- api/db_interface.py
+++ api/db_interface.py
@@ -28,4 +28,113 @@ class db_interface(object):
         new_user = User(email, username, password, '')
         self.users.add(new_user.to_dict())
         return (new_user, None)
+    
+    
+    #edit user with given info
+    def edit_user(self):
+        # TODO: implement
+    
+    
+    #delete user with username
+    def delete_user(self, username):
+        # TODO: implement
         
+        
+    #create a new post
+    def create_post(self):
+        # TODO: implement
+        
+        
+    #edit a post
+    def edit_post(self):
+        # TODO: implement
+    
+    
+    #delete a post
+    def delete_post(self):
+        # TODO: implement
+        
+    
+    #create a new comment
+    def create_comment(self):
+        # TODO: implement
+        
+    
+    #edit a comment
+    def edit_comment(self):
+        # TODO: implement
+        
+        
+    #delete comment
+    def delete_comment(self):
+        # TODO: implement
+        
+        
+    #send a message
+    def send_message(self):
+        # TODO: implement
+        
+        
+    #block a user
+    def block_user(self):
+        # TODO: implement
+        
+        
+    #change privacy setting
+    def change_privacy_setting(self):
+        # TODO: implement
+        
+    
+    #add a reaction
+    def add_reaction(self):
+        # TODO: implement
+        
+    
+    #save a post
+    def save_post(self):
+        # TODO: implement
+        
+        
+    #search for a user
+    def search_user(self):
+        # TODO: implement
+        
+    
+    #tag post with topic
+    def tag_post_with_topic(self):
+        # TODO: implement
+        
+    
+    #get timeline of a user
+    def get_timeline(self):
+        # TODO: implement
+        
+    
+    #get userline of a user
+    def get_userline(self):
+        # TODO: implement
+        
+    
+    #create a new topic
+    def create_topic(self):
+        # TODO: implement
+        
+    
+    #search for a topic
+    def search_topic(self):
+        # TODO: implement
+        
+    
+    #create a message thread between two users
+    def create_thread(self):
+        # TODO: implement
+        
+    
+    #get a message thread between two users
+    def get_thread(self):
+        # TODO: implement
+        
+    
+    #get message threads for a user
+    def get_threads(self):
+        # TODO: implement

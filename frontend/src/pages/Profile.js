51c75b8c6b4c4994b5864f492aa40f5ba0ba5ddf
nasr00 <70342009+nasr00@users.noreply.github.com>
2022-02-18 06:52:46 -0500

    Added profile page

---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Profile() {
+
+     return (
+      <div id="background">
+        <div id="profilePage">
+            <div id="userDisplay"></div>
+            <div id="nameDisplay"></div>
+            <div id="bioDisplay"></div>
+
+            <div id ="editProfButton">
+                <a href="/editProfile" class="button">Edit Profile</a>
+            </div>
+        </div>
+      </div>
+     );
+
+ }
+
+ export default Profile;

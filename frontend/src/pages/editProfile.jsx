32e5bf36ce67c1b152145345f265e775da37a30c
LRoswick <70717563+LRoswick@users.noreply.github.com>
2022-02-21 16:46:11 -0500

    fixed editProfile

--- frontend/src/pages/editProfile.jsx
+++ frontend/src/pages/editProfile.jsx
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
-import Editable from "./Editable"
-
 
 class editProfile extends React.Component {
 
     render() { 
 
-        const [task, setTask] = useState(" ");
-
         return ( 
-            <Editable
-                text={task}
-                placeholder="sampletext"
-                type="input"
-            >
-                <input
-                    type="text"
-                    name="task"
-                    placeholder="sampletext"
-                    value={task}
-                    onChange={e => setTask(e.target.value)}
-                />
-            </Editable>
+            <div id="background">
+            <div id="EditProfilePage">
+            <form>
+          <label>
+          Username:
+          <input type="text"/>
+          </label>
+          </form>
+    
+          <form>
+          <label>
+          Email:
+          <input type="text"/>
+          </label>
+          </form>
+    
+          <form>
+          <label>
+          Password:
+          <input type="password"/>
+          </label>
+          </form>
+      
+            <div id ="savebutton">
+                <a href="/" class="button">Save</a>
+            </div>
+
+            <div id ="backbutton">
+                <a href="/" class="button">Back</a>
+            </div>
+
+      
+            </div>
+            </div>
         );
     }
 }

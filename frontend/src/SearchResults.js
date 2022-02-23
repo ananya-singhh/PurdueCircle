8d684b000bf7b0a15435d7124d34e0a7ff248f39
cmccormick12 <mccorm61@purdue.edu>
2022-02-23 14:41:49 -0500

    search functionality implemented fully with backend

--- frontend/src/SearchResults.js
+++ frontend/src/SearchResults.js
@@ -1,15 +1,37 @@
-import { React } from 'react';
+import { React, useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import { useNavigate } from 'react-router-dom';
 import ListGroup from 'react-bootstrap/ListGroup';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
 
 
 function SearchResults() { 
   const navigate = useNavigate();
-  const list = ["conner", "conner2", "die"];
-  return (
+  const [list, setList] = useState([])
+  const query = useParams()['query'];
+  const url = window.location.pathname.split('/').pop();
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'http://127.0.0.1:5000/search_for_user?query=' + query,
+    }).then( res => {
+      if (res.data.data !== "No Results") {
+        setList(res.data)
+      } 
+    }).catch(error => {
+      //console.error(error);
+      //navigate("/404");
+    })
+  }, [url]);
+  
+  if(list.length > 0) {
+    console.log(list)
+    return (
     <Container className="App-pfpage">
-        <h1 Style="margin-top: 10px;"><strong>Search Results:</strong></h1>
+        <h1 Style="margin-top: 10px;"><strong>Search Results: </strong></h1>
+        
 
       <ListGroup variant="flush">
         {list.map((item) => (
@@ -18,6 +40,14 @@ function SearchResults() {
       </ListGroup>
     </Container>
     );
+  } else return (
+    <Container className="App-pfpage">
+        <h1 Style="margin-top: 10px;"><strong>No Results</strong></h1>
+      
+    </Container>
+  );
+  
+  
  
 }
 export default SearchResults;

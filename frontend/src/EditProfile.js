260e5609eb955ad57a54e7523a71842730266d8d
Adam N <nasr0@purdue.edu>
2022-02-23 22:59:27 -0500

    started linking profile editing to backend

--- frontend/src/EditProfile.js
+++ frontend/src/EditProfile.js
@@ -19,37 +19,44 @@ import pic4 from "./images/4.jpg";
 import pic5 from "./images/5.jpg";
 import pic6 from "./images/6.jpg";
 
-function changePfp(arg) {
-  console.log("clicked");
-  var elem = document.getElementById('pfp');
-  if(elem !== null && elem !== 'undefined' && elem.src !== null && elem.src !== 'undefined' && arg !== null) {
-    document.getElementById("pfp").src = arg;
-    imageToBeSaved = arg;
-  }
-}
+
 
 function EditProfile() { 
+  var user = JSON.parse(localStorage.getItem('user'));
 
-  const pics = [pic1, pic2, pic3, pic4, pic5, pic6];
+    const pics = [pic1, pic2, pic3, pic4, pic5, pic6];
 
-    const [edit, setEdit] = useState({bio: "", image: -1, privacy: -1});
+    const [edit, setEdit] = useState({bio: user['bio'], image: user['profile_picture'], privacy: -1});
     const navigate = useNavigate();
 
-    const user = JSON.parse(localStorage.getItem('user'));
     const username = useParams()['username'];
     console.log(user['username'] + " " + username);
 
     const [show, setShow] = useState(false);
 
-    const imageToBeSaved = pic1; //to be replaced with user's image by indexing list
-                                  //it will change when the user changes pfp using UI, so it can be referenced when saving changes to user data.
+    function changePfp(arg) {
+      console.log("clicked");
+      var elem = document.getElementById('pfp');
+      if(elem !== null && elem !== 'undefined' && elem.src !== null && elem.src !== 'undefined' && arg !== null) {
+        document.getElementById("pfp").src = arg;
+        //console.log(pics.indexOf(arg));
+        //setEdit({...edit, profile_picture: pics.indexOf(arg)});
+        edit.profile_picture = pics.indexOf(arg);
+        console.log(edit.profile_picture);
+      }
+    }
+    
+    function saveChanges() {
+      
+    }
+
 
   return (
     <Container className="App-pfpage">
     <Card className="text-center" bg="light" style={{ width: '18rem' }}>
     <Card.Body>
 
-    <FigureImage as={Image} width={125} height={125} src={pic1} roundedCircle={true} id="pfp" alt="Card image" /> 
+    <FigureImage as={Image} width={125} height={125} src={pic1} roundedCircle={true} id="pfp" alt="Card image"/> 
     <div style={{marginTop: 10}}>
     <DropdownButton title="Change Profile Picture" id="picsbar" flip={true}>
         <ListGroup variant="flush">
@@ -73,14 +80,11 @@ function EditProfile() {
 
     <Form.Label>{user['username']}</Form.Label>
 
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-      <Form.Label>Edit Email:</Form.Label>
-      <Form.Control type="email" placeholder="Enter an email" value={user['email']}/>
-    </Form.Group>
+
   
     <Form.Group className="mb-3" controlId="formBasicEmail">
       <Form.Label>Edit Bio:</Form.Label>
-      <Form.Control type="email" placeholder="Enter a bio" value={user['bio']} />
+      <Form.Control type="email" placeholder="Enter a bio" onChange = {e => setEdit({...edit, bio: e.target.value})} value={edit.bio}/>
     </Form.Group>
     
     <Form.Group className="mb-3" controlId="formProfileVisibility">
@@ -97,7 +101,7 @@ function EditProfile() {
     </Form.Group>
 
     <div class="col-sm-12 text-center">
-        <Button type= "submit" id="follow" class="btn btn-primary btn-md center-block" Style="width: 100px; margin-right: 25px;" OnClick="confirm" >Confirm</Button>
+        <Button type= "submit" id="follow" class="btn btn-primary btn-md center-block" Style="width: 100px; margin-right: 25px;" OnClick={saveChanges()} >Confirm</Button>
          <Button id="block" class="btn btn-danger btn-md center-block" Style="width: 100px;" onClick={() => navigate('/Profile/'+user['username'])}>Cancel</Button>
      </div>
 

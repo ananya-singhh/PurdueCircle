8c4cb42684a854986bdd5f560eb80713892cb85b
Adam N <nasr0@purdue.edu>
2022-02-23 22:23:13 -0500

    added profile pictures and UI for editing them

--- frontend/src/EditProfile.js
+++ frontend/src/EditProfile.js
@@ -6,8 +6,31 @@ import Button from 'react-bootstrap/Button';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import BootstrapSwitchButton from 'bootstrap-switch-button-react'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import FigureImage from 'react-bootstrap/FigureImage'
+import Image from 'react-bootstrap/Image'
+import ListGroup from 'react-bootstrap/ListGroup';
+
+import pic1 from "./images/1.jpg";
+import pic2 from "./images/2.jpg";
+import pic3 from "./images/3.jpg";
+import pic4 from "./images/4.jpg";
+import pic5 from "./images/5.jpg";
+import pic6 from "./images/6.jpg";
+
+function changePfp(arg) {
+  console.log("clicked");
+  var elem = document.getElementById('pfp');
+  if(elem !== null && elem !== 'undefined' && elem.src !== null && elem.src !== 'undefined' && arg !== null) {
+    document.getElementById("pfp").src = arg;
+    imageToBeSaved = arg;
+  }
+}
 
 function EditProfile() { 
+
+  const pics = [pic1, pic2, pic3, pic4, pic5, pic6];
 
     const [edit, setEdit] = useState({bio: "", image: -1, privacy: -1});
     const navigate = useNavigate();
@@ -16,12 +39,36 @@ function EditProfile() {
     const username = useParams()['username'];
     console.log(user['username'] + " " + username);
 
+    const [show, setShow] = useState(false);
 
+    const imageToBeSaved = pic1; //to be replaced with user's image by indexing list
+                                  //it will change when the user changes pfp using UI, so it can be referenced when saving changes to user data.
 
   return (
     <Container className="App-pfpage">
     <Card className="text-center" bg="light" style={{ width: '18rem' }}>
     <Card.Body>
+
+    <FigureImage as={Image} width={125} height={125} src={pic1} roundedCircle={true} id="pfp" alt="Card image" /> 
+    <div style={{marginTop: 10}}>
+    <DropdownButton title="Change Profile Picture" id="picsbar" flip={true}>
+        <ListGroup variant="flush">
+          {pics.map((item) => (
+          <ListGroup.Item action variant="light" onClick={() => changePfp(item)}><Dropdown.Item>
+          <FigureImage
+          width={50}
+          height={50}
+          alt="50x50"
+          src={item}
+          roundedCircle={true}
+          />
+        </Dropdown.Item>
+        <Dropdown.Divider /></ListGroup.Item>
+          ))}
+        </ListGroup>
+		</DropdownButton>
+    </div>
+        
     <Form>
 
     <Form.Label>{user['username']}</Form.Label>
@@ -43,7 +90,7 @@ function EditProfile() {
     checked={false}
     onlabel='Public'
     offlabel='Private'
-    onChange={(checked: boolean) => {
+    onChange={(checked) => {
             this.setState({ isUserAdmin: checked })
     }}
     />

72942363a7be669eb360dfb7ef790021a497caec
luke <lroswick@purdue.edu>
2022-03-03 13:14:26 -0500

    added TopicPage and SavedPosts page

---
+++ frontend/src/TopicPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+//import axios from 'axios';
+
+
+function TopicPage() { 
+        return (  
+        <Container className="App-pfpage">
+        <h1 Style="margin-top: 10px;"><strong>Topic Name</strong></h1>
+        </Container>
+        );
+}
+export default TopicPage;
+

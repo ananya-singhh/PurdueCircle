e4cb473ca8662317bd9654b1b496d905d4e604a8
luke <lroswick@purdue.edu>
2022-03-07 14:57:10 -0500

    import statements

--- frontend/src/TopicPage.js
+++ frontend/src/TopicPage.js
@@ -1,6 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import Container from 'react-bootstrap/Container';
-//import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+
+import Image from 'react-bootstrap/Image'
+import FigureImage from 'react-bootstrap/FigureImage'
+
+import Post from "./Post"
+
+import pic1 from "./images/1.jpg";
+import pic2 from "./images/2.jpg";
+import pic3 from "./images/3.jpg";
+import pic4 from "./images/4.jpg";
+import pic5 from "./images/5.jpg";
+import pic6 from "./images/6.jpg";
 
 
 function TopicPage() { 

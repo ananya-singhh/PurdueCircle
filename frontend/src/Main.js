623699c91beda177826e89a719685b64d81d653a
Adam N <nasr0@purdue.edu>
2022-03-22 21:29:04 -0400

    added functionality and navigation for creating posts

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Post from './Post';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 function Main() { 
  
   return (
   <Container className="App-main">
   <p>Main</p>
-  <Post />
+  <Row>
+  <Col xs={10}>
+    <Post />
+    <Post />
+    <Post />
+  </Col>
+  <Col>
+  <Button variant="primary" href="/CreatePost">Create a Post</Button>
+  </Col>
+  </Row>
   </Container>
   );
 

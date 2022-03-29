de9795f62651feffc3c8c490e817aa9903b6547c
Adam N <nasr0@purdue.edu>
2022-03-28 21:52:44 -0400

    editing and deleting posts is a thing now

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -4,6 +4,8 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
@@ -29,6 +31,7 @@ import CommentsPage from './CommentsPage';
 
         var id = props.id; // pass id to the post, ex. <Post id={"exampleID"}/>, the id in this line will be "exampleID"
         const [postInfo, setPostInfo] = useState({});
+        const currentUser = JSON.parse(localStorage.getItem('user'));
 
         const [checked, setChecked] = useState(false);
         const [commentsVisible, setCommentsVisible] = useState(false);
@@ -51,7 +54,32 @@ import CommentsPage from './CommentsPage';
             setCommentsVisible(!commentsVisible);
         }
 
-        useEffect(() => {
+        const [editMode, setEditMode] = useState(true);
+        const toggleEditMode = (e) => {
+            setEditMode(!editMode);
+        }
+        const saveEdits = (e) => {
+            setEditMode(!editMode);
+            axios({
+                method: 'put',
+                url: 'http://127.0.0.1:5000/edit_post',
+                data: {
+                    id: id,
+                    content: postInfo.content,
+                }
+              }).then( res => {
+
+              }).catch(error => {
+                console.error(error);
+                //navigate("/404");
+              })
+
+
+
+            getPost();
+        }
+
+        function getPost() {
             axios({
                 method: 'get',
                 url: 'http://127.0.0.1:5000/get_post?id=' + id,
@@ -63,17 +91,84 @@ import CommentsPage from './CommentsPage';
                 console.error(error);
                 //navigate("/404");
               })
+        }
+        useEffect(() => {
+            getPost();
         }, [id]);
+
+        const [show, setShow] = useState(false);
+        const handleNo = () => {
+            setShow(false);
+          }
+          const areYouSure = () => setShow(true);
+
+          const handleYes = () => { //TODO: DELETE THE ACC
+            setShow(false);
+            axios({
+              method: 'delete',
+              url: 'http://127.0.0.1:5000/delete_post?id=' + id,
+            }).then( res => {
+              
+              
+                
+            }).catch(error => {
+              console.log(error);
+              //navigate("/404");
+            })
+            window.location.reload(true);
+          }
 
         return (
             <Container className="App-post" Style="margin-bottom: 10px;">
             <Card className="text-left" bg="light">
-            <Card.Header>
+            
+            {postInfo['author'] === currentUser['username'] ?
+                <Card.Header>
+                <Row>
+                <Col>
                 <Card.Title>{postInfo['title'] ? postInfo['title'] : "Loading..."}</Card.Title> 
                 <Card.Subtitle>{postInfo['author'] ? "@" + postInfo['author'] : "Loading..."}</Card.Subtitle>
-            </Card.Header>
+                </Col>
+                
+
+                <Col md={{span: 1, offset: 3}}>
+                    {editMode ? 
+                    <Button variant="warning" onClick={toggleEditMode}>Edit</Button>
+                    :
+                    <Button onClick={saveEdits}>Save</Button> }
+                </Col>
+                <Col md={{ span: 2, offset: 1}}>
+                    <Button variant="danger" onClick={areYouSure}>Delete</Button>
+                    <Modal show={show} onHide={handleNo}>
+					<Modal.Header closeButton>
+					  <Modal.Title>Delete Post</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>Are you sure you want to delete this post?</Modal.Body>
+					<Modal.Footer>
+					  <Button variant="secondary" onClick={handleNo}>
+						No
+					  </Button>
+					  <Button variant="primary" onClick={handleYes}>
+						Yes
+					  </Button>
+					</Modal.Footer>
+					</Modal>
+                </Col>
+                </Row>
+                </Card.Header>
+                : 
+                <Card.Header>
+                <Card.Title>{postInfo['title'] ? postInfo['title'] : "Loading..."}</Card.Title> 
+                <Card.Subtitle>{postInfo['author'] ? "@" + postInfo['author'] : "Loading..."}</Card.Subtitle>
+                </Card.Header>
+                }
+        
             <Card.Body>
-                {postInfo['content'] ? postInfo['content'] : "Loading..."}
+                {editMode ? 
+                postInfo['content'] ? postInfo['content'] : "Loading..."
+                :
+                <Form.Control as="textarea" value={postInfo['content']} onChange = {e => setPostInfo({...postInfo, content: e.target.value})}/>
+                }
             
             </Card.Body>
             <Card.Footer>

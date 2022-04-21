6c1f05bb201b17144c01f03a92a152c48a3fe1d9
saisravanik <skuchibh@purdue.edu>
2022-04-21 00:37:23 -0400

    wont let me change branch without doing this

--- app.py
+++ app.py
@@ -275,10 +275,7 @@ def get_saved_posts():
     else:
         return {"data" : "failed"}
 
-<<<<<<< HEAD
-=======
 
->>>>>>> e50c9f2592b79de661c613885f9a7ddf431f6d1a
 @app.route('/get_following', methods=['GET'])
 def get_following():
     ret = db.get_following(request.args['username'])
@@ -286,10 +283,7 @@ def get_following():
         return json.dumps(ret)
     else:
         return {"data" : "failed"}
-<<<<<<< HEAD
-=======
 
->>>>>>> e50c9f2592b79de661c613885f9a7ddf431f6d1a
 @app.route('/create_message', methods=['POST'])
 def create_message():
     request_data = request.get_json()
@@ -322,10 +316,7 @@ def get_thread():
         user2 = None
     ret = db.get_thread(user1, user2, thread_id)
     return ret
-<<<<<<< HEAD
-=======
 
->>>>>>> e50c9f2592b79de661c613885f9a7ddf431f6d1a
 
 if __name__ == "__main__":
     app.run(debug=True)
